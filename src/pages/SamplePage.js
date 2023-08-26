5ebbe6ddc7e39850da6051584ec77d2c6a49cd51
Srishty M <srishtymangutte@gmail.com>
2023-08-26 19:37:18 +0530

    chore: made folder structure

---
+++ src/pages/SamplePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/sample.styles.css'
+
+const SamplePage = () => {
+  return (
+    <div className='container'>A SamplePage for example</div>
+  )
+}
+
+export default SamplePage;
