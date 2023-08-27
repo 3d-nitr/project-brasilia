0703cd88a81f81ad159efe23021dd13174e69428
Srishty M <srishtymangutte@gmail.com>
2023-08-28 03:02:45 +0530

    fix: depemdencies

--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './styles/sample.styles.css'
+import Navbar from '../components/marginals/Navbar';
 
 const SamplePage = () => {
   return (
+    <>
+    <Navbar/>
     <div className='container'>A SamplePage for example</div>
+    </>
   )
 }
 
