99398827b50798195a906ab61fa54b4efa31b2f3
Srishty M <srishtymangutte@gmail.com>
2023-08-28 02:06:34 +0530

    feat: added contact and register pages

---
+++ src/pages/Register.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/marginals/Navbar";
+import Footer from "../components/marginals/Footer";
+import Table from "../components/Table";
+
+const Register = () => {
+  return (
+    <>
+      <Navbar />
+      <Table />
+      <Footer />
+    </>
+  );
+};
+
+export default Register;
