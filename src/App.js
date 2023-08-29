bed847810a7ebcc599c1846a4f1368a8ec73c74d
Khair Alanam <anaambasheer@gmail.com>
2023-08-29 10:19:38 +0530

    Include topbar and navbar in all pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ContactUs from './pages/contact';
-import SamplePage from './pages/SamplePage.js';
-import Register from './pages/Register.js';
-import HomePage from './pages/HomePage';
-import React  from 'react';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContactUs from "./pages/contact";
+import SamplePage from "./pages/SamplePage.js";
+import Register from "./pages/Register.js";
+import HomePage from "./pages/HomePage";
+import React from "react";
+import TopBar from "./components/TopBar";
+import Navbar from "./components/marginals/Navbar";
 
 function App() {
   return (
-      <>
-        <BrowserRouter>
+    <>
+      <BrowserRouter>
+        <TopBar />
+        <Navbar />
         <Routes>
           <Route index path="/" element={<HomePage />} />
-          <Route path="/sp" element={<SamplePage/>} />
-          <Route path="/contact" element={<ContactUs/>} />
-          <Route path='/register' element={<Register />} />
+          <Route path="/sp" element={<SamplePage />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
-      </>
+    </>
   );
 }
 
